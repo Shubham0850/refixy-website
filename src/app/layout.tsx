cd32a75b0a08313b95f4dc25168148343198a27f
Shubham0850 <rajshubham0850@gmail.com>
2025-07-06 15:26:10 +0530

    fix: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -46,6 +47,7 @@ export default function RootLayout({
       <body
         className={"antialiased"}
       >
+        <Analytics />
         {children}
       </body>
     </html>
